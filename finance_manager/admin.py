7bdb27da4cfc59bf309528aed2eff80646f4ef36
Pozy <paul.jax@yandex.ru>
2022-12-24 20:58:21 +0500

    Основные модели добавлены в админ панель

--- finance_manager/admin.py
+++ finance_manager/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from finance_manager.models import FinanceManagerUser
+from finance_manager.models import FinanceManagerUser,\
+    Expenses, RegularExpenses, CashIncomes, Banks
 
 admin.site.register(FinanceManagerUser)
+admin.site.register(Expenses)
+admin.site.register(RegularExpenses)
+admin.site.register(CashIncomes)
+admin.site.register(Banks)
