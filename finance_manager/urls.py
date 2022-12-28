09e9dc9eaa35340c8d1b9487461a6797a0c542e7
Pozy <paul.jax@yandex.ru>
2022-12-28 15:24:55 +0500

    Добавлены основные url для регистрации, обновления пользователя

--- finance_manager/urls.py
+++ finance_manager/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from finance_manager.views import ExpensesAPIUpdate, ExpensesAPIList, FinanceUserRegisterAPIView, \
+    FinanceUserUpdateAPIView
 
+urlpatterns = [
+    path('register/', FinanceUserRegisterAPIView.as_view(), name='register'),
+    path('update/', FinanceUserUpdateAPIView.as_view(), name='update'),
+    path('expenseslist/', ExpensesAPIList.as_view(), name='expenses')
 ]
