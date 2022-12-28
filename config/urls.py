ce8189c25e0bd683564e24afba0337bf8d7010e9
Pozy <paul.jax@yandex.ru>
2022-12-28 15:24:24 +0500

    Добавлены основные url для получения, обновления, проверки токена

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('finance/', include('finance_manager.urls'))
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/finance_manager/', include('finance_manager.urls'))
 ]
