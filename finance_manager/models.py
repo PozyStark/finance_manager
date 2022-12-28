d1cf752a9e4182b45458d7801ceee288c10a5a40
Pozy <paul.jax@yandex.ru>
2022-12-28 15:19:37 +0500

    Переделана модель кастомного пользователя AbstractUser заменен на AbstractBaseUser

--- finance_manager/models.py
+++ finance_manager/models.py
@@ -1,69 +1,73 @@
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
 from django.db import models
-
-# TODO: "Добавить валидацию email не для супер пользователей"
+from django.contrib.auth.models import User, PermissionsMixin
 
 
 class FinanceUserManager(BaseUserManager):
+    """Менеджер для создания кастомного пользователя
+    """
 
-    def create_user(self, email, password, **extra_fields):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
         user.set_password(password)
-        user.save()
-
+        user.save(using=self._db)
         return user
 
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_stuff', True)
+        extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_stuff') is not True:
-            raise ValueError('Superuser has to have is_stuff being True')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_stuff=True')
 
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser has to have is_superuser being True')
+            raise ValueError('Superuser must have is_superuser=True')
 
-        return self.create_user(email=email, password=password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
 
-class FinanceManagerUser(AbstractUser):
-    """Модель пользователя с аутентификацией по email"""
+class FinanceUser(AbstractBaseUser, PermissionsMixin):
+    """Модель кастомного пользователя для авторизации по полю email
+    """
 
-    GENDERS = (
-        ('none', 'None'),
-        ('male', 'Male'),
-        ('female', 'Female')
-    )
+    email = models.EmailField(max_length=255, null=False, blank=False, unique=True, db_index=True)
+    number = models.CharField(max_length=18, null=True, blank=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(null=True, blank=True, auto_now=True)
 
-    objects = FinanceUserManager
+    objects = FinanceUserManager()
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
-
-    email = models.EmailField(max_length=255, null=False, blank=False, unique=True)
-    username = models.CharField(max_length=255, null=False, blank=False)
-    age = models.PositiveIntegerField(default=0, null=False, blank=False)
-    gender = models.CharField(max_length=255, choices=GENDERS, default=GENDERS[0], null=False, blank=False)
-    time_created = models.DateField(auto_now_add=True, null=False, blank=False)
-    time_updated = models.DateField(auto_now=True, null=False, blank=False)
-    last_login = models.DateField(auto_now=True, null=False, blank=False)
-
-    def __str__(self):
-        return self.email
+    REQUIRED_FIELDS = ['number']
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = 'FinanceUser'
+        verbose_name_plural = 'FinanceUsers'
 
 
 class Expenses(models.Model):
     """Модель содержит все возможные траты пользователя"""
 
-    user = models.ForeignKey('FinanceManagerUser', on_delete=models.CASCADE)
+    user = models.ForeignKey(FinanceUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=False, blank=False)
     amount = models.FloatField(default=0, null=False, blank=False)
-    date = models.DateField(auto_created=True)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user.email} [{self.name}] [{self.amount}]'
@@ -76,7 +80,7 @@ class Expenses(models.Model):
 class RegularExpenses(models.Model):
     """Модель содержит регулярные траты пользователя"""
 
-    user = models.ForeignKey('FinanceManagerUser', on_delete=models.CASCADE)
+    user = models.ForeignKey(FinanceUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=False, blank=False)
     amount = models.FloatField(default=0, null=False, blank=False)
 
@@ -91,7 +95,7 @@ class RegularExpenses(models.Model):
 class CashIncomes(models.Model):
     """Модель содержит все возможные доходы пользователя"""
 
-    user = models.ForeignKey('FinanceManagerUser', on_delete=models.CASCADE)
+    user = models.ForeignKey(FinanceUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=False, blank=False)
     amount = models.FloatField(default=0, null=False, blank=False)
     regular = models.BooleanField(default=False, null=False, blank=False)
@@ -108,7 +112,7 @@ class CashIncomes(models.Model):
 class Banks(models.Model):
     """Модель содержит цели для накопления"""
 
-    user = models.ForeignKey('FinanceManagerUser', on_delete=models.CASCADE)
+    user = models.ForeignKey(FinanceUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=False, blank=False)
     required_amount = models.FloatField(default=0, null=False, blank=False)
     available_amount = models.FloatField(default=0, null=False, blank=False)
