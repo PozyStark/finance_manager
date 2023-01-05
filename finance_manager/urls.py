55fa24dd5b9f7c7081ff61d21957c779c9f7a374
Pozy <paul.jax@yandex.ru>
2023-01-05 15:43:09 +0500

    Добавлены новые маршруты для регистрации, изменения профиля, смены пароля

--- finance_manager/urls.py
+++ finance_manager/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from finance_manager.views import ExpensesAPIUpdate, ExpensesAPIList, FinanceUserRegisterAPIView, \
-    FinanceUserUpdateAPIView
+from finance_manager.views import \
+    FinanceUserRegisterAPIView, \
+    FinanceUserProfileUpdateAPIView, \
+    FinanceUserChangePasswordAPIView, \
+    ExpensesAPIList, \
+    ExpenseDetailAPIView
 
 urlpatterns = [
     path('register/', FinanceUserRegisterAPIView.as_view(), name='register'),
-    path('update/', FinanceUserUpdateAPIView.as_view(), name='update'),
-    path('expenseslist/', ExpensesAPIList.as_view(), name='expenses')
+    path('updateprofile/', FinanceUserProfileUpdateAPIView.as_view(), name='update_profile'),
+    path('changepassword/', FinanceUserChangePasswordAPIView.as_view(), name='change_password'),
+
+    path('expenseslist/', ExpensesAPIList.as_view(), name='expenses_list'),
+    path('expensedetail/<int:id>/', ExpenseDetailAPIView.as_view(), name='expense_detail')
 ]
