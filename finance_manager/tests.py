05964ed975da0daa6b0c42a0dec0b5784709e590
Pozy <paul.jax@yandex.ru>
2023-02-18 18:37:51 +0500

    Создан unittest

--- finance_manager/tests.py
+++ finance_manager/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from models import User
 
-# Create your tests here.
+class BaseTest(TestCase):
+    def create_user(self):
+        user = User.objects.create(
+            email='test@mail.ru',
+            number='89121545772',
+            first_name='Paul',
+            last_name='Komarov',
+        )
+        user.set_password('1234567*')
+        self.assertTrue(user.save())
