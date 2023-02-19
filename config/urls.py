b09a91b7f347b0d3d5661bae2c6a9401c0f8c33b
Pozy <paul.jax@yandex.ru>
2023-02-19 17:05:59 +0500

    Исправлен endpoint для приложения

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/finance_manager/', include('finance_manager.urls'))
+    path('api/finance-manager/', include('finance_manager.urls'))
 ]
